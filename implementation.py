10f9118455521ae8668fe8edbdb7097961c4f729
David Boening <davidboening@yahoo.it>
2023-01-31 11:15:14 +0100

    added example notebook for each stage

--- implementation.py
+++ implementation.py
@@ -1,15 +1,12 @@
 # python libraries
 from numbers import Number
-from typing import Optional, Tuple
+from typing import Tuple, Optional
 
 # external dependencies
 import torch
 
-# not implemented modules
-from oracle import grid_interp, point_rasterize
 
-
-def minmax_scaling(points: torch.Tensor, eps=1e-8) -> torch.Tensor:
+def minmax_scaling(points: torch.Tensor, eps:float=1e-8) -> torch.Tensor:
     """Scale all points to be within [0,1]
 
     Parameters
@@ -28,7 +25,7 @@ def minmax_scaling(points: torch.Tensor, eps=1e-8) -> torch.Tensor:
     max = points.max(axis=1)[0].max() + eps
     return (points - min) / (max - min)
 
-def centerzoom_scaling(points:torch.Tensor, scale=1.0) -> torch.Tensor:
+def centerzoom_scaling(points:torch.Tensor, scale:float=1.0) -> torch.Tensor:
     """Center input in origin and shift in [-1,1] * scale.
 
     Parameters
@@ -72,7 +69,7 @@ def get_fft_frequencies(grid: Tuple[int, int, int]) -> torch.Tensor:
 
 
 def get_gaussian_smoothing(
-    input: torch.Tensor, sigma: Number = 5, res: Number = None
+    input: torch.Tensor, sigma: Number = 5, res: Optional[Number] = None
 ) -> torch.Tensor:
     """Returns a gaussian smoothing kernel
 
@@ -227,27 +224,117 @@ def point_rasterization(
 
 
 def grid_interpolation(
-    grid_of_values: torch.Tensor, points: torch.Tensor
+    field_values: torch.Tensor, query_points: torch.Tensor
 ) -> torch.Tensor:
-    """Given a function approximated by a grid of values, sampled uniformly
-    along each dimension (e.g. (32,32,32)). Approximates, for each point
-    given, the value the function assumes in said point.
-    The approximation is done via trilinear interpolation.
+    """Given a scalar/vector field approximated using a grid and a set
+    of query points returns field values for each query point approximated
+    using trilinear interpolation.
+    ATTENTION: query_points must be in [0, 1]
 
     Parameters
     ----------
-    grid_of_values : torch.Tensor
-        shape = [1, *grid]
-    points : torch.Tensor
-        shape = [1, npoints, len(grid)]
+    field_values : torch.Tensor
+        a batch of field grid with shape = [batch, *grid, nfeatures]
+    query_points : torch.Tensor
+        a batch of query points with shape = [batch, npoints, dim]
 
     Returns
     -------
     torch.Tensor
-        shape = [1, npoints]
+        field values at query points with shape = [batch, npoints, nfeatures]
     """
-    # TODO: implement
-    return grid_interp(grid_of_values.unsqueeze(-1), points).squeeze(-1)
+
+    batchsize = query_points.shape[0]
+    samplesize = query_points.shape[1]
+    dim = query_points.shape[2]
+    voxelcount = torch.tensor(field_values.shape[1:-1])
+    # s0, s1, s2
+    voxelsize = 1 / voxelcount
+    eps = 1e-5
+
+    # compute neighbor indices
+    lower_index = torch.floor(query_points / voxelsize).int()
+    outliers = torch.cat([
+        torch.argwhere(lower_index > voxelcount - eps),
+        torch.argwhere(lower_index < -eps)
+    ])
+    if len(outliers) > 0:
+        raise ValueError(f"{len(outliers)} points are outside grid limits")
+    # lower_index = lower_index.remainder(voxelcount)
+    
+    upper_index = torch.ceil(query_points / voxelsize).int()
+    outliers = torch.cat([
+        torch.argwhere(upper_index > voxelcount + 1 - eps),
+        torch.argwhere(upper_index < -eps)
+    ])
+    if len(outliers) > 0:
+        raise ValueError(f"{len(outliers)} points are outside grid limits")
+    upper_index = upper_index.fmod(voxelcount)
+
+    sample_index = torch.stack([lower_index, upper_index], dim=0)
+
+    # all subsets of 2**dim elements
+    # e.g. if dim=3 : 000, 001, 010, ..., 110, 111
+    combinations = torch.stack(
+        torch.meshgrid(
+            *([torch.tensor([0,1])] * dim), 
+            indexing="ij"
+        ), 
+        dim=-1
+    ).reshape(2**dim, dim)
+
+    # [0,1,..,dim-1] * (2**dim)
+    # e.g if dim=3 : [[1,2,3],[1,2,3],...]
+    selection = torch.arange(dim).repeat(2**dim, 1)
+
+    # creates all possible indexing combinations
+    # e.g. (low, low, low), (low, low, up), ..., (up, up, up)
+    # combinations generates all possible combinations:
+    # (xl, xl, xl), (xl, xl, xu), ...
+    # (yl, yl, yl), (yl, yl, yu), ...
+    # (zl, zl, zl), (zl, zl, zu), ...
+    # selection then selects the diagonal of each:
+    # -> (xl, yl, zl), (xl, yl, zu), (xl, yu, zl), ...
+    neighbor_index = sample_index[combinations, ..., selection]
+    sample_index = neighbor_index.permute(2, 3, 0, 1) # [batch, npoints, 2**dim, dim]
+
+    # similarly we construct the cube coordinates
+    lower_coords = lower_index * voxelsize
+    upper_coords = (lower_index+1) * voxelsize
+    sample_coords = torch.stack([lower_coords, upper_coords], dim=0)
+    # here we invert the order from upper to lower instead
+    neighbor_coords = sample_coords[1 - combinations, ..., selection]
+    neighbor_coords = neighbor_coords.permute(2, 3, 0, 1)
+
+    # distances from points to neighbors
+    neighbor_dist = torch.abs(query_points.unsqueeze(-2) - neighbor_coords)
+    # scale distances to use cubes as a metric
+    neighbor_dist /= voxelsize
+
+    # compute trilinear weights for all 8 (if dim=3) neighbors of each sample as:
+    # n0 = |x_{n0} - x|/sx * |y_{n0} - y|/sy * |z_{n0} - z|/sz * (feature_value)
+    weights = torch.prod(neighbor_dist, dim=-1, keepdim=False)
+    
+    # initialize batch and feature indices
+    batch_index = torch.arange(batchsize).expand(samplesize, 2**dim, batchsize)
+    batch_index = batch_index.permute(2, 0, 1)
+
+    # get neighbor values
+    # NOTE: if python version is 3.11 or higher this if can be removed and
+    # value unpacking in subscripts was added
+    if dim == 2:
+        x,y = tuple(sample_index[..., i] for i in range(dim))
+        neighbor_values = field_values[batch_index, x, y]
+    elif dim == 3:
+        x,y,z = tuple(sample_index[..., i] for i in range(dim))
+        neighbor_values = field_values[batch_index, x, y, z]
+    else:
+        raise NotImplementedError("dim > 3 until transition to python 3.11")
+
+    # weighted sum over neighbor values
+    query_values = torch.sum(weights.unsqueeze(-1) * neighbor_values, dim=-2)
+
+    return query_values
 
 
 def DPSR_forward(self, points: torch.Tensor, normals: torch.Tensor) -> torch.Tensor:
@@ -257,42 +344,44 @@ def DPSR_forward(self, points: torch.Tensor, normals: torch.Tensor) -> torch.Ten
     Parameters
     ----------
     points : torch.Tensor
-        a sample of points (shape = [1, npoints, 3])
+        a sample of points (shape = [batch, npoints, 3])
         currently all values must be within (0,1)
     normals : torch.Tensor
-        normals of said points (shape = [1, npoints, 3])
+        normals of said points (shape = [batch, npoints, 3])
 
     Returns
     -------
     torch.Tensor
         approximation of the indicator function
-        shape = [1, *grid]
+        shape = [batch, *grid]
     """
     ### NOTE: I'm not 100% sure but I believe this implementation requires
     ### points to be in (0,1) given how "\chi' restricted to x=0" is computed
 
     # compute vector v
-    v = point_rasterization(points, normals, self.grid)  # [1, 3, 32, 32, 32]
+    v = point_rasterization(points, normals, self.grid)  # [batch, dim, *grid]
     # compute vector v^~
     v_tilde = torch.fft.fftn(v, dim=(2, 3, 4))
-    v_tilde = v_tilde.permute([0, 2, 3, 4, 1])  # [1, 32, 32, 32, 3]
+    v_tilde = v_tilde.permute([0, 2, 3, 4, 1])  # [batch, *grid, dim]
     # compute vector u
-    u = get_fft_frequencies(self.grid).unsqueeze(0)  # [1, 32, 32, 32, 3]
+    u = get_fft_frequencies(self.grid).unsqueeze(0)  # [batch, *grid, dim]
     # compute vector g^~_{\sigma,r}(u)
-    g = get_gaussian_smoothing(u, self.sigma, self.grid[0])  # [1, 32, 32, 32]
+    g = get_gaussian_smoothing(u, self.sigma, self.grid[0])  # [batch, *grid]
     # compute scalar -i/2pi
     _scalar = -1j / (2 * torch.pi)
     # compute vector u @ v^~ / |u|^2
-    _vector = torch.sum(u * v_tilde, dim=-1)  # dot-product  # [1, 32, 32, 32]
-    _vector /= torch.sum(u**2, dim=-1) + self.eps  # [1, 32, 32, 32]
+    _vector = torch.sum(u * v_tilde, dim=-1)  # dot-product  # [batch, *grid]
+    _vector /= torch.sum(u**2, dim=-1) + self.eps  # [batch, *grid]
     # compute vector \chi^~
-    chi_tilde = g * (_scalar * _vector)  # [1, 32, 32, 32]
+    chi_tilde = g * (_scalar * _vector)  # [batch, *grid]
     # compute vector \chi'
-    chi_prime = torch.fft.ifftn(chi_tilde, dim=(1, 2, 3))  # [1, 32, 32, 32]
+    chi_prime = torch.fft.ifftn(chi_tilde, dim=(1, 2, 3))  # [batch, *grid]
     chi_prime = chi_prime.real  # imag is zero
 
     # compute vector \chi' restricted to x=c
-    chi_c = grid_interpolation(chi_prime, points)
+    chi_c = grid_interpolation(
+        chi_prime.unsqueeze(-1), points
+    ).squeeze(-1)                                          # [batch, *grid]
     _vector = chi_prime - torch.mean(chi_c, dim=-1)
     # compute scalar \chi' restricted to x=0
     chi_0 = _vector[:, 0, 0, 0]
